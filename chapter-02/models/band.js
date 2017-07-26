c79ef0e9cda1a8be918bc832fa6551a613e604a5
agramar <iamsjh1017@gmail.com>
2017-07-26 09:53:45 +0900

    Chapter02 - MySQL 데이터베이스를 이용한 기본 웹사이트 만들기

---
+++ chapter-02/models/band.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function (sequelize, DataTypes) {
+    var Band = sequelize.define('Band', {
+        name: DataTypes.STRING,
+        description: DataTypes.STRING,
+        album: DataTypes.STRING,
+        year: DataTypes.STRING,
+        UserId: DataTypes.INTEGER
+    }, {
+        classMethods: {
+            associate: function (models) {
+                // 관계 정의는 여기서 한다.
+                Band.belongsTo(models.User);
+            }
+        }
+    });
+    return Band;
+};
